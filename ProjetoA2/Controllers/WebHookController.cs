9399041cf077c9f416660a0b8c152d3d60948cbe
Eduardo Rocha <Eduardo Rocha@DUNDUN>
2023-12-08 22:18:09 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoA2/Controllers/WebHookController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoA2.Models;
+using ProjetoA2.Hubs;
+
+namespace ProjetoA2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebHookController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public WebHookController(IHubContext<ChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async void receberDadosWebHook(PagamentoPix pagamentoPix)
+        {
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", pagamentoPix.pix[0].txid, pagamentoPix.pix[0].valor);
+        }
+
+        [HttpGet]
+        public async void get()
+        {
+        }
+    }
+}
---
+++ ProjetoA2/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using ProjetoA2.Controllers;
+
+namespace ProjetoA2.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user)
+        {
+            // await Clients.All.SendAsync("ReceiveMessage", user);
+        }
+    }
+}
